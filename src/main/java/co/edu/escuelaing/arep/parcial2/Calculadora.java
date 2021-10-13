35570e4e927f1a7f8575452db204cf1d3aa6ddca
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2021-10-13 14:42:42 -0500

    aplicacion funcionando local

---
+++ src/main/java/co/edu/escuelaing/arep/parcial2/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arep.parcial2;
+
+
+/**
+ *
+ * @author jgarc
+ * 
+ */
+
+public class Calculadora {
+    
+    public Double log(Double value){
+        return Math.log10(value); 
+    }
+
+    public Double asin(Double value){
+        return  Math.asin(value);
+    }
+}
---
+++ src/main/java/co/edu/escuelaing/arep/parcial2/spark/SparkServer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arep.parcial2.spark;
+
+/**
+ *
+ * @author jgarc
+ */
+import static spark.Spark.*;
+import co.edu.escuelaing.arep.parcial2.Calculadora;
+import org.json.simple.JSONObject;
+
+
+public class SparkServer 
+{
+    public static void main( String[] args )
+    {
+        JSONObject json = new JSONObject(); 
+        port(getPort());
+        get("/log", (req, res) -> {
+            Double value = Double.parseDouble(req.queryParams("value"));
+            Calculadora calculator = new Calculadora();
+            json.put("operation", "log" ); 
+            json.put("input", value ); 
+            json.put("output" , calculator.log(value) ); 
+            return json;
+        });
+        
+        get("/asin", (req,res) -> {
+            Double value = Double.parseDouble(req.queryParams("value"));
+            Calculadora calculator = new Calculadora();
+            json.put("operation", "log" ); 
+            json.put("input", value ); 
+            json.put("output" , calculator.asin(value) ); 
+            return calculator.asin(value);
+        });
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
